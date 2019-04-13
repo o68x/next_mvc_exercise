47f2f0b600b5b4c157c9ce16ac860874064548c0
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-04-13 18:20:57 +0200

    Update seed with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
-Category.create!(name: "Good")
+Category.create!(name: "That's fine")
 
-Category.create!(name: "Bad")
+Category.create!(name: "Well")
+
+Category.create!(name: "Just a category")
 
 1.upto(20) do |i|
   item = Item.create!(
     name: Faker::Artist.name,
     original_price: Faker::Number.decimal(2),
-    has_discount: Faker::Boolean.boolean
+    has_discount: Faker::Boolean.boolean,
+    categories: [Category.all.sample]
   )
   if item.has_discount
     item.discount_percentage = (1..10).to_a.sample * 5
